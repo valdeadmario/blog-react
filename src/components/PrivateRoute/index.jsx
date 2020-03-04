0222f0fadf54d74433493f035ae9446428c71852
valdeadmario <vladimir.vstup@gmail.com>
2020-03-04 21:34:33 +0200

    Delete redux dev-tools

--- src/components/PrivateRoute/index.jsx
+++ src/components/PrivateRoute/index.jsx
@@ -5,11 +5,7 @@ import { Route, Redirect } from "react-router-dom";
 const PrivateRoute = props => {
   const isAuthorized = useSelector(state => state.auth.isAuthorized);
 
-  return isAuthorized ? (
-    <Route {...props} />
-  ) : (
-    <Redirect to="/login" />
-  );
+  return isAuthorized ? <Route {...props} /> : <Redirect to="/login" />;
 };
 
 export default PrivateRoute;
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,4 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
 
 import authReducer from "../components/Login/reducer";
 import newsReducer from "../components/News/reducer";
@@ -8,7 +7,7 @@ const initialState = {};
 
 const middlewares = [];
 
-const composedEnhancers = composeWithDevTools(applyMiddleware(...middlewares));
+const composedEnhancers = applyMiddleware(...middlewares);
 
 const reducers = {
   auth: authReducer,
